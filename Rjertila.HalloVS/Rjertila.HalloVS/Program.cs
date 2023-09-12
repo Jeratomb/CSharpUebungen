5f2c1379ab049be074df14d775e8701c26452e5c
rjertila <Rayen.Jertila@edvschule-plattling.de>
2023-09-12 14:43:11 +0200

    In-/Output es Vornamens

---
+++ Rjertila.HalloVS/Rjertila.HalloVS/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Bitte gib deinen Namen ein");
+string vorname = Console.ReadLine()?? String.Empty;
+Console.WriteLine($"Hallo {vorname}, schön, dass du C# programmierst");
