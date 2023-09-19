85854d1509c6c677a394ca3aa1d0b7bc9692c7fc
rjertila <Rayen.Jertila@edvschule-plattling.de>
2023-09-19 13:53:50 +0200

    test

--- Rjertila.BMI/Rjertila.BMI/Program.cs
+++ Rjertila.BMI/Rjertila.BMI/Program.cs
@@ -3,7 +3,7 @@
 using Rjertila.BMI;
 using System.ComponentModel.DataAnnotations;
 
-Person[] array = new Person[3];
+Person[] array = new Person[4];
 array[0] = new Person("Kugelblitz", "Klaus", rjertila.bmi.Gender.Male, new DateOnly(1981, 02, 19));
 array[1] = new Person("Müsli", "Martina", rjertila.bmi.Gender.Female, new DateOnly(1984,07,25));
 array[2] = new Person("Groß", "Günther", rjertila.bmi.Gender.Male, new DateOnly(1996,02,29));
@@ -15,7 +15,7 @@ foreach(Person p in array)
     Console.WriteLine("--------------------------");
 }
 		
-
+// Die foreach-Schleife greift auf eine Instanz zurück und die for-Schleife nur auf den Index des arrays
 
 
 
