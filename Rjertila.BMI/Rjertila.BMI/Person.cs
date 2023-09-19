d3c7c866a52ad82b9842ae748b8c84cd105b2bee
rjertila <Rayen.Jertila@edvschule-plattling.de>
2023-09-19 16:12:36 +0200

    Almost Done

--- Rjertila.BMI/Rjertila.BMI/Person.cs
+++ Rjertila.BMI/Rjertila.BMI/Person.cs
@@ -1,6 +1,7 @@
 ﻿using rjertila.bmi;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Rjertila.BMI
 {
 	internal class Person
 	{
+		#region Propfulls
+
 		private string? _Nachname;
 
 		public string? Nachname
@@ -23,6 +26,7 @@ namespace Rjertila.BMI
 				_Nachname = value;
 			}
 		}
+
 
 		private string? _Vorname;
 
@@ -44,7 +48,7 @@ namespace Rjertila.BMI
 		public Gender Geschlecht
 		{
 			get { return _Geschlecht; }
-			set {	_Geschlecht = value; 	}
+			set { _Geschlecht = value; }
 		}
 
 
@@ -85,24 +89,53 @@ namespace Rjertila.BMI
 				_GroesseCm = value;
 			}
 		}
+		#endregion
 
-		public Person(string nachname,string vorname, Gender geschlecht, DateOnly geburtsdatum )
+		#region GetOnlyProps
+		private string VollerName
 		{
-			Nachname = nachname;
-			Vorname = vorname;
-			Geschlecht = geschlecht;
-			Geburtsdatum = geburtsdatum;
+			get { return Vorname + Nachname; }
 		}
+
+		private int AktuellesAlter
+		{
+			get { return BerechneAlter(DateOnly.FromDateTime(DateTime.Now)); }
+		}
+
+		private bool IstVolljaehrig
+		{
+			get { return 18 <= AktuellesAlter; }
+		}
+		#endregion
+
+		#region Constructors
 
 		public Person()
 		{
 			Nachname = String.Empty;
 			Vorname = String.Empty;
 			Geschlecht = Gender.Male;
-			Geburtsdatum = new DateOnly(1900,01,01);
+			Geburtsdatum = new DateOnly(1900, 01, 01);
 			GroesseCm = null;
 			GewichtKg = null;
 		}
+		public Person(string nachname, string vorname, Gender geschlecht, DateOnly geburtsdatum)
+			: this()
+		{
+			this.Nachname = nachname;
+			this.Vorname = vorname;
+			this.Geschlecht = geschlecht;
+			this.Geburtsdatum = geburtsdatum;
+		}
+
+		public Person(string nachname, string vorname, Gender geschlecht, DateOnly geburtsdatum, int? groessecm = null, double? gewichtkg = null)
+			: this(nachname, vorname, geschlecht, geburtsdatum)
+		{
+			GroesseCm = groessecm;
+			GewichtKg = gewichtkg;
+		}
+
+		#endregion
 
 		public string getGender()
 		{
@@ -111,17 +144,45 @@ namespace Rjertila.BMI
 				case Gender.Male:
 					return "Männlich";
 				case Gender.Female:
-					return "Weiblich";				
+					return "Weiblich";
 				case Gender.Diverse:
-					return "Divers";				
+					return "Divers";
 				default:
 					return "NoGender";
 			}
 		}
 
+		public int BerechneAlter(DateOnly day)
+		{
+
+			int years = day.Year - this.Geburtsdatum.Year;
+			if (day < this.Geburtsdatum)
+			{
+				return 0;
+			}
+			else
+			{
+				if (this.Geburtsdatum.Month <= day.Month)
+				{
+					if (this.Geburtsdatum.Day < day.Day)
+					{
+						years--;
+					}
+					else
+					{
+						if (this.Geburtsdatum.Year % 400 == 0 &&(this.Geburtsdatum.Month == 02 && this.Geburtsdatum.Day == 29 && this.Geburtsdatum.Day > day.Day))
+						{
+							years--;
+						}
+					}
+				}
+			}
+			return years;
+		}
+
 		public override string ToString()
 		{
-			return $"Vorname: {Vorname} \n Nachname: {Nachname} \n Geschlecht: {getGender()} \n Geburtsdatum: {Geburtsdatum} \n GroesseCm: {GroesseCm} \n GewichtKg: {GewichtKg}";
+			return String.Format("Vorname: {0} \n Nachname: {1} \n Geschlecht: {2} \n Geburtsdatum: {3} \n GroesseCm: {4} \n GewichtKg: {5}", Vorname, Nachname, getGender(), Geburtsdatum, GroesseCm, GewichtKg);
 
 		}
 	}
--- Rjertila.BMI/Rjertila.BMI/Program.cs
+++ Rjertila.BMI/Rjertila.BMI/Program.cs
@@ -1,21 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Rjertila.BMI;
-using System.ComponentModel.DataAnnotations;
 
-Person[] array = new Person[4];
-array[0] = new Person("Kugelblitz", "Klaus", rjertila.bmi.Gender.Male, new DateOnly(1981, 02, 19));
-array[1] = new Person("Müsli", "Martina", rjertila.bmi.Gender.Female, new DateOnly(1984,07,25));
-array[2] = new Person("Groß", "Günther", rjertila.bmi.Gender.Male, new DateOnly(1996,02,29));
+Person[] array = new Person[3];
+array[0] = new Person("Kugelblitz", "Klaus", rjertila.bmi.Gender.Male, new DateOnly(1981, 02, 19), 157, 96.3);
+array[1] = new Person("Müsli", "Martina", rjertila.bmi.Gender.Female, new DateOnly(1984,07,25), 175);
+array[2] = new Person("Groß", "Günther", rjertila.bmi.Gender.Male, new DateOnly(1996,02,29),203, 74.6);
+DateOnly now = new DateOnly(2023, 09, 19);
+Person test = new Person("s", "asds", rjertila.bmi.Gender.Male, new DateOnly(2000, 02, 29));
 
 foreach(Person p in array)
 {
+    Console.WriteLine(p.BerechneAlter(now));
     Console.WriteLine(p);
     Console.WriteLine("--------------------------");
 }
-		
-
-
-
-
-
+Console.WriteLine(test.BerechneAlter(new DateOnly(2003,03,1)));
