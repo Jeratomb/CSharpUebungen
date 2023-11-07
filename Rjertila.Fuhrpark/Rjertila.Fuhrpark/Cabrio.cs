e4d57f76d5c01f7622b1b0a6cb1644a8d0ac39fa
rjertila <Rayen.Jertila@edvschule-plattling.de>
2023-11-07 16:10:39 +0100

    done

---
+++ Rjertila.Fuhrpark/Rjertila.Fuhrpark/Cabrio.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rjertila.Fuhrpark;
+
+	public class Cabrio : Auto
+	{
+	public bool IstVerdeckOffen { get; private set; }
+
+	public Cabrio(string hersteller, string typ, int zulassungsjahr, double listenpreis, int maximaleGeschwindigkeit, bool istVerdeckOffen)
+			: base(hersteller, typ, zulassungsjahr, listenpreis, maximaleGeschwindigkeit)
+	{
+		this.IstVerdeckOffen = istVerdeckOffen;
+	}
+
+	public void OeffnenVerdeck()
+	{
+		IstVerdeckOffen = true;
+	}
+
+	public void SchliessenVerdeck()
+	{
+		IstVerdeckOffen = false;
+	}
+	protected override double GetBeschleunigung()
+	{
+
+		return 4;
+	}
+
+	protected override double GetBremsbeschleunigung()
+	{
+
+		return 7;
+	}
+
+	public override double BerechneWert(int Jahr)
+	{
+		double decreaseRate;
+		double decValue = this.Listenpreis;
+		int yearRange = Jahr - this.Zulassungsjahr;
+
+		if (yearRange <= 0)
+		{
+			return this.Listenpreis;
+		}
+		else if (yearRange <= 10)
+		{
+			decreaseRate = 0.08; 
+			
+			for (int i = 0; i < yearRange; i++)
+			{
+				decValue -= decValue * decreaseRate;
+			}
+			return Math.Ceiling(decValue);
+		}
+		else if (yearRange <= 20)
+		{
+			decreaseRate = 0.06; 
+			for (int i = 0; i < yearRange; i++)
+			{
+				decValue -= decValue * decreaseRate;
+			}
+			return Math.Ceiling(decValue);
+		}
+		else
+		{
+			return this.Listenpreis; 
+		}
+	}
+}
+
--- Rjertila.Fuhrpark/Rjertila.Fuhrpark/Program.cs
+++ Rjertila.Fuhrpark/Rjertila.Fuhrpark/Program.cs
@@ -1,2 +1,71 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Rjertila.Fuhrpark
+{
+	class Program
+	{
+		static void BeschleunigungsTest(Cabrio cabrio)
+		{
+			Console.WriteLine($"Aktuelle Geschwindigkeit des Cabrios: {cabrio.AktuelleGeschwindigkeit} km/h");
+			cabrio.Beschleunigen(10);
+			Console.WriteLine("Beschleunigen für 10 Sekunden");
+			Console.WriteLine($"Aktuelle Geschwindigkeit des Cabrios nach Beschleunigung: {cabrio.AktuelleGeschwindigkeit} km/h");
+		}
+
+		static void BremsenTest(Cabrio cabrio)
+		{
+			cabrio.Bremsen(5);
+			Console.WriteLine($"Aktuelle Geschwindigkeit des Cabrios nach Bremsen: {cabrio.AktuelleGeschwindigkeit} km/h");
+		}
+
+		static void VerdeckTest(Cabrio cabrio)
+		{
+			cabrio.OeffnenVerdeck();
+			Console.WriteLine($"Ist das Verdeck des Cabrios geöffnet? {cabrio.IstVerdeckOffen}");
+
+			cabrio.SchliessenVerdeck();
+			Console.WriteLine($"Ist das Verdeck des Cabrios geschlossen? {cabrio.IstVerdeckOffen}");
+		}
+
+		static void WertBerechnungTest(Cabrio cabrio)
+		{
+			double wert = cabrio.BerechneWert(2023);
+			Console.WriteLine($"Wert des Cabrios im Jahr 2023: {wert}€");
+		}
+
+		static void BeschleunigungsTest(Auto auto)
+		{
+			auto.Beschleunigen(10);
+			Console.WriteLine($"Aktuelle Geschwindigkeit des Autos nach Beschleunigung: {auto.AktuelleGeschwindigkeit} km/h");
+		}
+
+		static void BremsenTest(Auto auto)
+		{
+			auto.Bremsen(5);
+			Console.WriteLine($"Aktuelle Geschwindigkeit des Autos nach Bremsen: {auto.AktuelleGeschwindigkeit} km/h");
+		}
+
+		static void WertBerechnungTest(Auto auto)
+		{
+			double autoWert = auto.BerechneWert(2021);
+			Console.WriteLine($"Wert des Autos im Jahr 2021: {autoWert}€");
+		}
+
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+			Cabrio cabrio = new Cabrio("Hersteller", "Cabrio", 2020, 50000, 200, false);
+			BeschleunigungsTest(cabrio);
+			BremsenTest(cabrio);
+			VerdeckTest(cabrio);
+			WertBerechnungTest(cabrio);
+
+			Auto auto = new Auto("Hersteller", "Auto", 2019, 50000, 180);
+			BeschleunigungsTest(auto);
+			BremsenTest(auto);
+			WertBerechnungTest(auto);
+		}
+
+	}
+}
