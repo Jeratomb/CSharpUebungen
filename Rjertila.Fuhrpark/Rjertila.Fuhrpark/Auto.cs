f5b112c260a2681b6c0e4caf6ba3f7b53a4705e5
rjertila <Rayen.Jertila@edvschule-plattling.de>
2023-11-07 08:49:01 +0100

    added class Auto

---
+++ Rjertila.Fuhrpark/Rjertila.Fuhrpark/Auto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rjertila.Fuhrpark;
+public class Auto
+{
+	private String myVar;
+
+	public String MyProperty
+	{
+		get { return myVar; }
+		set { myVar = value; }
+	}
+
+}
