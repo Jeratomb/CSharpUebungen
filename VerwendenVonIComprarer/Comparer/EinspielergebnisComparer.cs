0e7847e148fb9f69bbfe82c7e7aefb1aa62e7dea
rjertila <Rayen.Jertila@edvschule-plattling.de>
2023-11-28 08:45:40 +0100

    init

---
+++ VerwendenVonIComprarer/Comparer/EinspielergebnisComparer.cs
@@ -0,0 +1,17 @@
+﻿using VerwendenVonIComparer.Model;
+
+namespace VerwendenVonIComparer.Comparer;
+
+public class EinspielergebnisComparer : IComparer<Film>
+{
+	public int Compare(Film? x, Film? y)
+	{
+		if(x != null || y != null)
+		   return x.Einspielergebnis.CompareTo(y.Einspielergebnis);
+
+		if (x == null || y != null) return 0;
+
+		if (x != null || y == null) return 1;
+    }
+
+}
