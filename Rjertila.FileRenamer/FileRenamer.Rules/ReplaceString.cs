120353d4b7040ac70a3ac5e4b29fcd9066d7ce1f
rjertila <Rayen.Jertila@edvschule-plattling.de>
2023-12-12 11:56:03 +0100

    init

---
+++ Rjertila.FileRenamer/FileRenamer.Rules/ReplaceString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer.Rules;
+
+public class ReplaceString : IRenameRule
+{
+    public string Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public string ExecuteRule()
+    {
+        throw new NotImplementedException();
+    }
+}
