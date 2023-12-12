63edb546b9f41a2ccab68ae89c14aaa7c9611e9b
rjertila <Rayen.Jertila@edvschule-plattling.de>
2023-12-12 12:15:26 +0100

    file strucutre

---
+++ Rjertila.FileRenamer/Rjertila.FileRenamer/IFileRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer.DataAccess;
+
+public interface IFileRepository
+{
+   void addFile(FileInfo file);
+   void deleteFile(FileInfo file);
+   List<FileInfo> getAllFiles();
+}
---
+++ Rjertila.FileRenamer/Rjertila.FileRenamer/IRenameRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer.Rules;
+
+public interface IRenameRule
+{
+    string Type { get; set; }
+    string Description { get; set; }
+    string ExecuteRule();
+}
